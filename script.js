bdadb11db69bf801a7e520a4a722d6ac1c3c0581
N.Upendhar <80301044+Upendhar10@users.noreply.github.com>
2025-07-23 16:41:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,53 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function(){
+
+	const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
+	const buttonsCont = document.getElementById('buttons');
+	
+	DisplayButtons(buttonsCont, sounds);
+
+	StopBtn(buttonsCont);
+})
+
+function DisplayButtons(buttonsCont, sounds){
+
+	sounds.forEach((sound) => {
+		const btn = document.createElement('button');
+		btn.classList.add('btn');
+		btn.innerText = sound;
+
+		SoundBtnClick(btn);
+		buttonsCont.appendChild(btn);
+	})
+}
+
+function SoundBtnClick(btn){
+	btn.addEventListener("click", () => {
+		StopAllSounds();
+		const audio = new Audio(`sounds/${sound}.mp3`);
+		audio.play();
+		audio.appendChild(audio);
+	})
+}
+
+function StopAllSounds(){
+	const allAudios = document.querySelectorAll('audio');
+
+	allAudios.forEach((audio) => {
+		audio.pause();
+		audio.currentTime = 0;
+		audio.remove();
+	})
+}
+
+function StopBtn(buttonsCont){
+	const stopBtn = document.createElement('button');
+	stopBtn.classList.add('stop');
+	stopBtn.innerText = "Stop";
+
+	stopBtn.addEventListener("click", () => {
+		StopAllSounds();
+	})
+
+	buttonsCont.appendChild(stopBtn);
+}
